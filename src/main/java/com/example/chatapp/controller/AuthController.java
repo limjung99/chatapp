e4c6c7029c69b8bb59a2be465adaf85cdc89b10c
limjung99 <limjung99@naver.com>
2024-06-19 17:20:54 +0900

    init

---
+++ src/main/java/com/example/chatapp/controller/AuthController.java
@@ -0,0 +1,10 @@
+package com.example.chatapp.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+}
---
+++ src/main/java/com/example/chatapp/entity/User.java
@@ -0,0 +1,4 @@
+package com.example.chatapp.entity;
+
+public class User {
+}
