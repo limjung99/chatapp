c0afe3c2852708d0b67d6a97e76bdf7ce406a512
limjung99 <limjung99@naver.com>
2024-07-06 16:53:10 +0900

    Feat : jwt 토큰 페어 생성

--- src/main/java/com/example/chatapp/util/JwtProvider.java
+++ src/main/java/com/example/chatapp/util/JwtProvider.java
@@ -1,31 +1,65 @@
 package com.example.chatapp.util;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 import com.example.chatapp.dto.TokenResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
+
 import java.net.Authenticator;
+import java.security.interfaces.RSAPrivateCrtKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Date;
 
 @Component
+@RequiredArgsConstructor
 public class JwtProvider {
 
-    private final String secret;
-    private final long timeoutMilliSecond;
-    private final UserDetailsService userDetailsService;
-    public JwtProvider(@Value("${jwt.secret}") String secret,
-                       @Value("${jwt.timeout}") long tokenTimeoutSecond,
-                       UserDetailsService userDetailsService){
-        this.secret = secret;
-        this.timeoutMilliSecond = tokenTimeoutSecond;
-        this.userDetailsService = userDetailsService;
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.time}")
+    private long expireTimeDelta;
+    private UserDetailsService userDetailsService;
+    @Value("${jwt.rsa-pub-key}")
+    private RSAPublicKey rsaPublicKey;
+    @Value("${jwt.rsa-private-key}")
+    private RSAPrivateKey rsaPrivateKey;
+
+    private String createToken(Algorithm algorithm, Authentication authentication) throws JWTCreationException{
+        return JWT.create()
+                .withExpiresAt(new Date(System.currentTimeMillis() + expireTimeDelta))
+                .withSubject(authentication.getName())
+                .sign(algorithm);
     }
-    public TokenResponseDto createToken(Authentication authenticaton) {
-        // TODO
+
+    private String createRefreshToken(Algorithm algorithm, Authentication authentication) throws JWTCreationException{
+        return JWT.create()
+                .withExpiresAt(new Date(System.currentTimeMillis() + expireTimeDelta*10))
+                .withSubject(authentication.getName())
+                .sign(algorithm);
+    }
+
+    public TokenResponseDto createTokenPair(Authentication authenticaton) throws JWTCreationException {
+        try {
+            Algorithm algorithm = Algorithm.RSA256(rsaPublicKey, rsaPrivateKey);
+            String token = createToken(algorithm, authenticaton);
+            String refreshToken = createRefreshToken(algorithm, authenticaton);
+            return TokenResponseDto.builder()
+                    .token(token)
+                    .refreshToken(refreshToken)
+                    .build();
+        } catch (JWTCreationException exception){
+            throw exception;
+        }
     }
 
     public boolean validateToken(String token){
-        // TODO
     }
 }
